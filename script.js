e0237b54bf255ed40d20e6b560055955cf8f479f
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-17 18:54:42 -0400

    create the showError and showSuccess functions

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Functions
+showError = (input, message) => {
+  // Change the class of the div
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  // Change the text of the error message
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+showSuccess = (input) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
 
 // Event listeners
 form.addEventListener('submit', (e) => {
