71e0564e659230fd7805db50702d109bcd836a94
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-17 19:06:16 -0400

    create a validateEmail function that check if the email is the correct form (text@text.com), and display a specific error message if it doesn't

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-// Functions
-showError = (input, message) => {
+const showError = (input, message) => {
   // Change the class of the div
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
@@ -14,34 +13,43 @@ showError = (input, message) => {
   small.innerText = message;
 }
 
-showSuccess = (input) => {
+const showSuccess = (input) => {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
+
+// Check if email is valid
+validateEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // Matches an email adress
+  return re.test(String(email).toLowerCase()); // Run a test method to see if it's matching, returns true/false
+}
+
 
 // Event listeners
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if(!username.value) {
+  if (!username.value) {
     showError(username, 'Username is required');
   } else {
     showSuccess(username);
   }
 
-  if(!email.value) {
+  if (!email.value) {
     showError(email, 'Email is required');
+  } else if (!validateEmail(email.value)) {
+    showError(email, 'Email is not valid');
   } else {
     showSuccess(email);
   }
 
-  if(!password.value) {
+  if (!password.value) {
     showError(password, 'Password is required');
   } else {
     showSuccess(password);
   }
 
-  if(!password2.value) {
+  if (!password2.value) {
     showError(password2, 'Password2 is required');
   } else {
     showSuccess(password2);
